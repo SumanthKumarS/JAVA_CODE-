8a88e33f938ec60bedfc7cefcf3f4638192c5709
SumanthKumarS <sumanthks.is18@sahyadri.edu.in>
2022-08-03 11:30:28 +0530

    Student class

---
+++ Student.java
@@ -0,0 +1,41 @@
+public class Student {
+    int id;
+    String name;
+    String gender;
+    int age;
+    long phone;
+    double gpa;
+    char degree;
+
+    boolean international;
+    double tuitionFees = 12000.0;
+    double interationalFees = 5000.0;
+    void compute(){
+        int nextId = id+1;
+        if(international){
+            tuitionFees = tuitionFees + interationalFees;
+        }
+        System.out.println("id"+id);
+        System.out.println("next_id"+nextId);
+        System.out.println("name"+name);
+        System.out.println("gender"+gender);
+        System.out.println("age"+age);
+        System.out.println("phone"+phone);
+        System.out.println("gpa"+gpa);
+        System.out.println("degree"+degree);
+        System.out.println("tuitionFees"+tuitionFees);
+    }
+
+    public static void main(String[] args) {
+        Student s = new Student();
+        s.id = 1000;
+        s.name = "John";
+        s.gender = "male";
+        s.age = 18;
+        s.phone = 223_245_7890L;
+        s.gpa = 3.8;
+        s.degree = 'B';
+        s.international = false;
+        s.compute();
+    }
+}
