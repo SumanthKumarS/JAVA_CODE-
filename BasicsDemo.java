0cc89f0b379634b8943e11da4148d0c8aa103ad2
SumanthKumarS <sumanthks.is18@sahyadri.edu.in>
2022-08-03 10:17:09 +0530

    updated Bsic demo

--- BasicsDemo.java
+++ BasicsDemo.java
@@ -1,11 +1,24 @@
 public class BasicsDemo {
     
-    
+    int idl = 1000;
+    byte age =1_8;
+    short rank = 165;
+    long phone = 123_456_7811L;
+    int minValue = Integer.MIN_VALUE;
+    int maxValue = Integer.MAX_VALUE;
+    byte miValue = Byte.MIN_VALUE;
+    byte maValue = Byte.MAX_VALUE;
     void compute(){
-        int idl = 1000;
+        
         int next_idl = idl+1;
         System.out.println(idl);
         System.out.println(next_idl);
+        System.out.println(age);
+        System.out.println(phone);
+        System.out.println(minValue);
+        System.out.println(maxValue);
+        System.out.println(miValue);
+        System.out.println(maValue);
     }
 
     public static void main(String[] args) {
