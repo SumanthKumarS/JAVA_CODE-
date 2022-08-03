0e2e1aeb78d1e2e9c657a9a65ae6488c0d2c827b
SumanthKumarS <sumanthks.is18@sahyadri.edu.in>
2022-08-03 09:55:47 +0530

    First commit

--- BasicsDemo.java
+++ BasicsDemo.java
@@ -1,3 +1,16 @@
 public class BasicsDemo {
-    int idl = 0;
+    
+    
+    void compute(){
+        int idl = 1000;
+        int next_idl = idl+1;
+        System.out.println(idl);
+        System.out.println(next_idl);
+    }
+
+    public static void main(String[] args) {
+        BasicsDemo bd = new BasicsDemo();
+        bd.compute();
+    }
+    
 }
